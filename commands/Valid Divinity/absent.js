38b3c339b3502d90a06f62406d626d22a8dd169e
WallyThePenguin <32379213+WallyThePenguin@users.noreply.github.com>
2017-12-23 23:59:09 -0800

    Absent Command created for absent notices.

---
+++ commands/Valid Divinity/absent.js
@@ -0,0 +1,40 @@
+const { Command } = require('klasa')
+const config = require('../../config/config.json')
+const absentrole = [
+  '393505548558925854'
+];
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'absent',
+      enabled: true,
+      runIn: ['text', 'dm', 'group'],
+      cooldown: 0,
+      aliases: ['abs'],
+      permLevel: 0,
+      botPerms: ['MANAGE_ROLES'],
+      requiredSettings: [],
+      description: 'For Valid Divinity use only',
+      quotedStringSupport: false,
+      usage: '<reason|urgent> <reas:str> <author:member> [...]',
+      usageDelim: ' ',
+      extendedHelp: 'No extended help available.'
+    })
+  }
+
+  async run (msg, [author, abs, ...reas]) {
+    if (msg.guild.id !== '393927898144440332') return msg.channel.send('Sorry this is a command only meant for EZL Staff Server.')
+    const final = await reas.join(' ')
+    switch (abs) {
+      case 'reason':
+        await this.client.channels.get('394352377647333386').send(final)
+        return msg.channel.send('Worked, Cya l8er Alligator!')
+        break
+        return msg.channel.send('Sorry, this is a EZL Command Only :smiley:')
+    }
+  }
+
+  async init () {
+    // You can optionally define this method which will be run when the bot starts (after login, so discord data is available via this.client)
+  }
+}
